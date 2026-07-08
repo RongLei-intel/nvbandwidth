ff8f6c1db550f9aadc0949f864cf1832111664c8
Rong, Lei <lei.rong@intel.com>
2026-07-08 16:40:27 +0000

    Switch sweep_test_3times.sh to t17 sweep config

--- sweep_test_3times.sh
+++ sweep_test_3times.sh
@@ -1,11 +1,13 @@
-bash sweep_config.sh
+bash sweep_config_t17.sh
 
 sleep 5s
 
-mv out out_7_6
+mv out out_7_9_v1
 
-bash sweep_config.sh
+bash sweep_config_t17.sh
 
-mv out out_7_6_v2
+mv out out_7_9_v2
 
-bash sweep_config.sh
+bash sweep_config_t17.sh
+
+mv out out_7_9_v3
